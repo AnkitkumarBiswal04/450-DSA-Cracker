5f4d0e60a0324c5f83c72208826e0154a9b566c9
Ankit Kumar Biswal <116702754+AnkitkumarBiswal04@users.noreply.github.com>
2024-06-08 23:00:15 +0530

    Update ReversetheArray.cpp

--- Arrays/ReversetheArray.cpp
+++ Arrays/ReversetheArray.cpp
@@ -1,31 +1,38 @@
-#include <iostream>;
+#include<iostream>
 using namespace std;
-void reverseArray(int arr[], int start, int end)
-{
-    while (start < end) {
-        int temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
+//this function is to reverse the array
+void reverse(int arr[],int n){
+    int start=0;
+    int end=n-1;
+    while(start<=end){
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
         start++;
         end--;
     }
 }
-void printArray(int arr[], int size)
-{
-    for (int i = 0; i < size; i++)
-        cout << arr[i] << " ";
-    cout << endl;
+//this function is to print the array
+void print(int arr[],int n){
+    cout<<"Reversed Array :";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
 }
-int main()
-{
-    int arr[] = { 1, 2, 3, 4, 5, 6 };
-    int n = sizeof(arr) / sizeof(arr[0]);
-    // To print original array
-    printArray(arr, n);
-    // Function calling
-    reverseArray(arr, 0, n - 1);
-    cout << "Reversed array is" << endl;
-    // To print the Reversed array
-    printArray(arr, n);
+//this is the main function
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"original array :";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    reverse(arr,n);
+    print(arr,n);
     return 0;
 }
